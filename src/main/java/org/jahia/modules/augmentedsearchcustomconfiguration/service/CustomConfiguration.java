1d7370910020e3fd78ec18e05d326594e8e75542
Geofrey Flores <gflores@jahia.com>
2026-04-02 09:48:40 -0400

    chore: Update to include all settings file in CustomConfiguration

--- src/main/java/org/jahia/modules/augmentedsearchcustomconfiguration/service/CustomConfiguration.java
+++ src/main/java/org/jahia/modules/augmentedsearchcustomconfiguration/service/CustomConfiguration.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * Provides custom mappings and settings for Augmented Search indexation
@@ -61,7 +62,8 @@ public class CustomConfiguration implements ESConfigService {
 
     @Override
     public JSONObject getSettingsForLanguage(String language) {
-        return ("ja".equals(language) || "pl".equals(language)) ?
+        // Return en to reproduce issue in AS 4.0.0 - See https://github.com/Jahia/jahia-private/issues/4856
+        return (List.of("en", "ja", "pl").contains(language)) ?
                 getConfigResource(String.format("/settings_%s.json", language)) : null;
     }
 
